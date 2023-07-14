04ad3ec5573f3cc2a4b61431b9330d5bf3e11e74
sangfgw <sang050901i@gmail.com>
2023-07-14 22:56:28 +0700

    Exercise Week 6 Updated

--- js/index.js
+++ js/index.js
@@ -1,161 +1,161 @@
 /* ------------------------------ 1 ------------------------------ */
 /* Sum of Two Number */
-const SUM_SUBMIT_BTN = document.getElementById("sum_submit");
-SUM_SUBMIT_BTN.addEventListener("click", (ev) => {
-    const SUM_FIRST_NUM = document.getElementById("sum_first_number").value == "" ? null : Number(document.getElementById("sum_first_number").value);
-    const SUM_SECOND_NUM = document.getElementById("sum_second_number").value == "" ? null : Number(document.getElementById("sum_second_number").value);
-    const SUM_RESULT_DIV = document.getElementById("sum_result");
-    if (SUM_FIRST_NUM != null && SUM_SECOND_NUM != null) {
-        SUM_RESULT_DIV.style.color = "green";
-        SUM_RESULT_DIV.innerHTML = `Result: ${SUM_FIRST_NUM} + ${SUM_SECOND_NUM} = ${SUM_FIRST_NUM + SUM_SECOND_NUM}`;
+const sumSubmitBtn = document.getElementById("sum_submit");
+sumSubmitBtn.addEventListener("click", (ev) => {
+    const sumFirstNum = document.getElementById("sum_first_number").value == "" ? null : Number(document.getElementById("sum_first_number").value);
+    const sumSecondNum = document.getElementById("sum_second_number").value == "" ? null : Number(document.getElementById("sum_second_number").value);
+    const sumResultDiv = document.getElementById("sum_result");
+    if (sumFirstNum != null && sumSecondNum != null) {
+        sumResultDiv.style.color = "green";
+        sumResultDiv.innerHTML = `Result: ${sumFirstNum} + ${sumSecondNum} = ${sumFirstNum + sumSecondNum}`;
     }
     else {
-        SUM_RESULT_DIV.style.color = "red";
-        SUM_RESULT_DIV.innerHTML = `Empty Input Fields or Invalid Data`;
+        sumResultDiv.style.color = "red";
+        sumResultDiv.innerHTML = `Empty Input Fields or Invalid Data`;
     }
 });
 
 /* ------------------------------ 2 ------------------------------ */
 /* C = r x 2 x 3,14 */
-const CIRCLE_CIRCUMFERENCE_SUBMIT_BTN = document.getElementById("circle_circumference_submit");
-CIRCLE_CIRCUMFERENCE_SUBMIT_BTN.addEventListener("click", (ev) => {
-    const RADIUS_NUMBER = document.getElementById("radius_number").value == "" ? null : Number(document.getElementById("radius_number").value);
-    const CIRCLE_CIRCUMFERENCE_RESULT_DIV = document.getElementById("circle_circumference_result");
-    if (RADIUS_NUMBER != null && RADIUS_NUMBER > 0) {
-        CIRCLE_CIRCUMFERENCE_RESULT_DIV.style.color = "green";
-        CIRCLE_CIRCUMFERENCE_RESULT_DIV.innerHTML = `Result: ${(RADIUS_NUMBER * 2) * Math.PI}`;
+const circleCircumferenceSubmitBtn = document.getElementById("circle_circumference_submit");
+circleCircumferenceSubmitBtn.addEventListener("click", (ev) => {
+    const radiusNumber = document.getElementById("radius_number").value == "" ? null : Number(document.getElementById("radius_number").value);
+    const circleCircumferenceResultDiv = document.getElementById("circle_circumference_result");
+    if (radiusNumber != null && radiusNumber > 0) {
+        circleCircumferenceResultDiv.style.color = "green";
+        circleCircumferenceResultDiv.innerHTML = `Result: ${(radiusNumber * 2) * Math.PI}`;
     }
-    else if (RADIUS_NUMBER != null && RADIUS_NUMBER <= 0) {
-        CIRCLE_CIRCUMFERENCE_RESULT_DIV.style.color = "red";
-        CIRCLE_CIRCUMFERENCE_RESULT_DIV.innerHTML = `Invalid Data: R must be (> 0)`;
+    else if (radiusNumber != null && radiusNumber <= 0) {
+        circleCircumferenceResultDiv.style.color = "red";
+        circleCircumferenceResultDiv.innerHTML = `Invalid Data: R must be (> 0)`;
     }
     else {
-        CIRCLE_CIRCUMFERENCE_RESULT_DIV.style.color = "red";
-        CIRCLE_CIRCUMFERENCE_RESULT_DIV.innerHTML = `Empty Input Field or Invalid Data`;
+        circleCircumferenceResultDiv.style.color = "red";
+        circleCircumferenceResultDiv.innerHTML = `Empty Input Field or Invalid Data`;
     }
 });
 
 /* ------------------------------ 3 ------------------------------ */
 /* If number % 2 = 0 => even, otherwise odd */
-const PARITY_CHECKING_SUBMIT_BTN = document.getElementById("parity_checking_submit");
-PARITY_CHECKING_SUBMIT_BTN.addEventListener("click", (ev) => {
-    const PARITY_NUMBER = document.getElementById("parity_number").value == "" ? null : Number(document.getElementById("parity_number").value);
-    const PARITY_CHECKING_RESULT_DIV = document.getElementById("parity_checking_result");
+const parityCheckingSubmitBtn = document.getElementById("parity_checking_submit");
+parityCheckingSubmitBtn.addEventListener("click", (ev) => {
+    const parityNumber = document.getElementById("parity_number").value == "" ? null : Number(document.getElementById("parity_number").value);
+    const parityCheckingResultDiv = document.getElementById("parity_checking_result");
 
-    if (PARITY_NUMBER != null) {
-        if (PARITY_NUMBER % 2 == 0) {
-            PARITY_CHECKING_RESULT_DIV.style.color = "green";
-            PARITY_CHECKING_RESULT_DIV.innerHTML = `Result: ${PARITY_NUMBER} is even number`;
+    if (parityNumber != null) {
+        if (parityNumber % 2 == 0) {
+            parityCheckingResultDiv.style.color = "green";
+            parityCheckingResultDiv.innerHTML = `Result: ${parityNumber} is even number`;
         }
         else {
-            PARITY_CHECKING_RESULT_DIV.style.color = "green";
-            PARITY_CHECKING_RESULT_DIV.innerHTML = `Result: ${PARITY_NUMBER} is odd number`;
+            parityCheckingResultDiv.style.color = "green";
+            parityCheckingResultDiv.innerHTML = `Result: ${parityNumber} is odd number`;
         }
     }
     else {
-        PARITY_CHECKING_RESULT_DIV.style.color = "red";
-        PARITY_CHECKING_RESULT_DIV.innerHTML = `Empty Input Field or Invalid Data`;
+        parityCheckingResultDiv.style.color = "red";
+        parityCheckingResultDiv.innerHTML = `Empty Input Field or Invalid Data`;
     }
 });
 
 /* ------------------------------ 4 ------------------------------ */
 /* Concatenate String */
-const CONCAT_STRING_SUBMIT_BTN = document.getElementById("concat_string_submit");
-CONCAT_STRING_SUBMIT_BTN.addEventListener("click", (ev) => {
-    const CONCAT_FIRST_STRING = String(document.getElementById("concat_first_string").value).trim();
-    const CONCAT_SECOND_STRING = String(document.getElementById("concat_second_string").value).trim();
-    const CONCAT_STRING_RESULT_DIV = document.getElementById("concat_string_result");
-    if (CONCAT_FIRST_STRING != "" && CONCAT_SECOND_STRING != "") {
-        CONCAT_STRING_RESULT_DIV.style.color = "green";
-        CONCAT_STRING_RESULT_DIV.innerHTML = `Result: ${CONCAT_FIRST_STRING} ${CONCAT_SECOND_STRING}`;
+const concatStringSubmitBtn = document.getElementById("concat_string_submit");
+concatStringSubmitBtn.addEventListener("click", (ev) => {
+    const concatFirstString = String(document.getElementById("concat_first_string").value).trim();
+    const concatSecondString = String(document.getElementById("concat_second_string").value).trim();
+    const concatStringResultDiv = document.getElementById("concat_string_result");
+    if (concatFirstString != "" && concatSecondString != "") {
+        concatStringResultDiv.style.color = "green";
+        concatStringResultDiv.innerHTML = `Result: ${concatFirstString} ${concatSecondString}`;
     }
     else {
-        CONCAT_STRING_RESULT_DIV.style.color = "red";
-        CONCAT_STRING_RESULT_DIV.innerHTML = `Empty Input Fields or Invalid Data`;
+        concatStringResultDiv.style.color = "red";
+        concatStringResultDiv.innerHTML = `Empty Input Fields or Invalid Data`;
     }
 });
 
 /* ------------------------------ 5 ------------------------------ */
 /* One Word Contain Check */
-const ONE_WORD_STRING_SUBMIT_BTN = document.getElementById("one_word_string_submit");
-ONE_WORD_STRING_SUBMIT_BTN.addEventListener("click", (ev) => {
-    const ONE_WORD_STRING = String(document.getElementById("one_word_string").value).trim();
-    const ONE_WORD_STRING_RESULT_DIV = document.getElementById("one_word_string_result");
-    if (ONE_WORD_STRING != "" && ONE_WORD_STRING.indexOf(' ') == -1) {
-        ONE_WORD_STRING_RESULT_DIV.style.color = "green";
-        ONE_WORD_STRING_RESULT_DIV.innerHTML = `Result: ${ONE_WORD_STRING} (is one word)`;
+const oneWOrdStringSubmitBtn = document.getElementById("one_word_string_submit");
+oneWOrdStringSubmitBtn.addEventListener("click", (ev) => {
+    const oneWordString = String(document.getElementById("one_word_string").value).trim();
+    const oneWordStringResultDiv = document.getElementById("one_word_string_result");
+    if (oneWordString != "" && oneWordString.indexOf(' ') == -1) {
+        oneWordStringResultDiv.style.color = "green";
+        oneWordStringResultDiv.innerHTML = `Result: ${oneWordString} (is one word)`;
     }
-    else if (ONE_WORD_STRING != "" && ONE_WORD_STRING.indexOf(' ') >= 0) {
-        ONE_WORD_STRING_RESULT_DIV.style.color = "darkorange";
-        ONE_WORD_STRING_RESULT_DIV.innerHTML = `Result: ${ONE_WORD_STRING} (is a paragraph)`;
+    else if (oneWordString != "" && oneWordString.indexOf(' ') >= 0) {
+        oneWordStringResultDiv.style.color = "darkorange";
+        oneWordStringResultDiv.innerHTML = `Result: ${oneWordString} (is a paragraph)`;
     }
     else {
-        ONE_WORD_STRING_RESULT_DIV.style.color = "red";
-        ONE_WORD_STRING_RESULT_DIV.innerHTML = `Empty Input Field or Invalid Data`;
+        oneWordStringResultDiv.style.color = "red";
+        oneWordStringResultDiv.innerHTML = `Empty Input Field or Invalid Data`;
     }
 });
 
 /* ------------------------------ 6 ------------------------------ */
 /* Empty String Check */
-const EMPTY_STRING_SUBMIT_BTN = document.getElementById("empty_string_submit");
-EMPTY_STRING_SUBMIT_BTN.addEventListener("click", (ev) => {
-    const EMPTY_STRING_VALUE = String(document.getElementById("empty_string").value).trim();
-    const EMPTY_STRING_RESULT_DIV = document.getElementById("empty_string_result");
-    if (EMPTY_STRING_VALUE != "") {
-        EMPTY_STRING_RESULT_DIV.style.color = "red";
-        EMPTY_STRING_RESULT_DIV.innerHTML = `Result: ${EMPTY_STRING_VALUE} is not an empty string`;
+const emptyStringSubmitBtn = document.getElementById("empty_string_submit");
+emptyStringSubmitBtn.addEventListener("click", (ev) => {
+    const emptyStringValue = String(document.getElementById("empty_string").value).trim();
+    const emptyStringResultDiv = document.getElementById("empty_string_result");
+    if (emptyStringValue != "") {
+        emptyStringResultDiv.style.color = "red";
+        emptyStringResultDiv.innerHTML = `Result: ${emptyStringValue} is not an empty string`;
     }
     else {
-        EMPTY_STRING_RESULT_DIV.style.color = "green";
-        EMPTY_STRING_RESULT_DIV.innerHTML = `Empty Input Field`;
+        emptyStringResultDiv.style.color = "green";
+        emptyStringResultDiv.innerHTML = `Empty Input Field`;
     }
 });
 
 /* ------------------------------ 7 ------------------------------ */
 /* Positive, Negative Number Check */
-const POSITIVE_NEGATIVE_NUMBER_SUBMIT_BTN = document.getElementById("positive_negative_number_submit");
-POSITIVE_NEGATIVE_NUMBER_SUBMIT_BTN.addEventListener("click", (ev) => {
-    const POSITIVE_NEGATIVE_NUMBER = document.getElementById("positive_negative_number").value == "" ? null : Number(document.getElementById("positive_negative_number").value);
-    const POSITIVE_NEGATIVE_NUMBER_RESULT_DIV = document.getElementById("positive_negative_number_result");
+const positiveNegativeNumberSubmitBtn = document.getElementById("positive_negative_number_submit");
+positiveNegativeNumberSubmitBtn.addEventListener("click", (ev) => {
+    const positiveNegativeNumber = document.getElementById("positive_negative_number").value == "" ? null : Number(document.getElementById("positive_negative_number").value);
+    const positiveNegativeNumberResultDiv = document.getElementById("positive_negative_number_result");
 
-    if (POSITIVE_NEGATIVE_NUMBER != null && POSITIVE_NEGATIVE_NUMBER > 0) {
-        POSITIVE_NEGATIVE_NUMBER_RESULT_DIV.style.color = "green";
-        POSITIVE_NEGATIVE_NUMBER_RESULT_DIV.innerHTML = `Result: ${POSITIVE_NEGATIVE_NUMBER} is a positive number`;
+    if (positiveNegativeNumber != null && positiveNegativeNumber > 0) {
+        positiveNegativeNumberResultDiv.style.color = "green";
+        positiveNegativeNumberResultDiv.innerHTML = `Result: ${positiveNegativeNumber} is a positive number`;
     }
-    else if (POSITIVE_NEGATIVE_NUMBER != null && POSITIVE_NEGATIVE_NUMBER == 0) {
-        POSITIVE_NEGATIVE_NUMBER_RESULT_DIV.style.color = "darkorange";
-        POSITIVE_NEGATIVE_NUMBER_RESULT_DIV.innerHTML = `Result: ${POSITIVE_NEGATIVE_NUMBER} is a neutral number`;
+    else if (positiveNegativeNumber != null && positiveNegativeNumber == 0) {
+        positiveNegativeNumberResultDiv.style.color = "darkorange";
+        positiveNegativeNumberResultDiv.innerHTML = `Result: ${positiveNegativeNumber} is a neutral number`;
     }
-    else if (POSITIVE_NEGATIVE_NUMBER != null && POSITIVE_NEGATIVE_NUMBER < 0) {
-        POSITIVE_NEGATIVE_NUMBER_RESULT_DIV.style.color = "green";
-        POSITIVE_NEGATIVE_NUMBER_RESULT_DIV.innerHTML = `Result: ${POSITIVE_NEGATIVE_NUMBER} is a negative number`;
+    else if (positiveNegativeNumber != null && positiveNegativeNumber < 0) {
+        positiveNegativeNumberResultDiv.style.color = "green";
+        positiveNegativeNumberResultDiv.innerHTML = `Result: ${positiveNegativeNumber} is a negative number`;
     }
     else {
-        POSITIVE_NEGATIVE_NUMBER_RESULT_DIV.style.color = "red";
-        POSITIVE_NEGATIVE_NUMBER_RESULT_DIV.innerHTML = `Empty Input Field or Invalid Data`;
+        positiveNegativeNumberResultDiv.style.color = "red";
+        positiveNegativeNumberResultDiv.innerHTML = `Empty Input Field or Invalid Data`;
     }
 });
 
 /* ------------------------------ 8 ------------------------------ */
 /* Sum, Subtraction, Multiplication, Divide Calculator */
-const COMPOSITE_CALCULATION_NUMBER_SUBMIT_BTN = document.getElementById("composite_calculation_number_submit");
-COMPOSITE_CALCULATION_NUMBER_SUBMIT_BTN.addEventListener("click", (ev) => {
-    const COMPOSITE_CALCULATION_FIRST_NUMBER = document.getElementById("composite_calculation_first_number").value == "" ? null : Number(document.getElementById("composite_calculation_first_number").value);
-    const COMPOSITE_CALCULATION_SECOND_NUMBER = document.getElementById("composite_calculation_second_number").value == "" ? null : Number(document.getElementById("composite_calculation_second_number").value);
-    const COMPOSITE_CALCULATION_NUMBER_RESULT_DIV = document.getElementById("composite_calculation_number_result");
+const compositeCalculationNumberSubmitBtn = document.getElementById("composite_calculation_number_submit");
+compositeCalculationNumberSubmitBtn.addEventListener("click", (ev) => {
+    const compositeCalculationFirstNumber = document.getElementById("composite_calculation_first_number").value == "" ? null : Number(document.getElementById("composite_calculation_first_number").value);
+    const compositeCalculationSecondNumber = document.getElementById("composite_calculation_second_number").value == "" ? null : Number(document.getElementById("composite_calculation_second_number").value);
+    const compositeCalculationNumberResultDiv = document.getElementById("composite_calculation_number_result");
 
-    if (COMPOSITE_CALCULATION_FIRST_NUMBER != null && COMPOSITE_CALCULATION_SECOND_NUMBER != null) {
-        COMPOSITE_CALCULATION_NUMBER_RESULT_DIV.style.color = "green";
-        COMPOSITE_CALCULATION_NUMBER_RESULT_DIV.innerHTML = 
+    if (compositeCalculationFirstNumber != null && compositeCalculationSecondNumber != null) {
+        compositeCalculationNumberResultDiv.style.color = "green";
+        compositeCalculationNumberResultDiv.innerHTML = 
         `
-            <div>${COMPOSITE_CALCULATION_FIRST_NUMBER} + ${COMPOSITE_CALCULATION_SECOND_NUMBER} = ${COMPOSITE_CALCULATION_FIRST_NUMBER + COMPOSITE_CALCULATION_SECOND_NUMBER}</div>
-            <div>${COMPOSITE_CALCULATION_FIRST_NUMBER} - ${COMPOSITE_CALCULATION_SECOND_NUMBER} = ${COMPOSITE_CALCULATION_FIRST_NUMBER - COMPOSITE_CALCULATION_SECOND_NUMBER}</div>
-            <div>${COMPOSITE_CALCULATION_FIRST_NUMBER} * ${COMPOSITE_CALCULATION_SECOND_NUMBER} = ${COMPOSITE_CALCULATION_FIRST_NUMBER * COMPOSITE_CALCULATION_SECOND_NUMBER}</div>
-            <div>${COMPOSITE_CALCULATION_FIRST_NUMBER} / ${COMPOSITE_CALCULATION_SECOND_NUMBER} = ${COMPOSITE_CALCULATION_FIRST_NUMBER / COMPOSITE_CALCULATION_SECOND_NUMBER}</div>
+            <div>${compositeCalculationFirstNumber} + ${compositeCalculationSecondNumber} = ${compositeCalculationFirstNumber + compositeCalculationSecondNumber}</div>
+            <div>${compositeCalculationFirstNumber} - ${compositeCalculationSecondNumber} = ${compositeCalculationFirstNumber - compositeCalculationSecondNumber}</div>
+            <div>${compositeCalculationFirstNumber} * ${compositeCalculationSecondNumber} = ${compositeCalculationFirstNumber * compositeCalculationSecondNumber}</div>
+            <div>${compositeCalculationFirstNumber} / ${compositeCalculationSecondNumber} = ${compositeCalculationFirstNumber / compositeCalculationSecondNumber}</div>
         `;
     }
     else {
-        COMPOSITE_CALCULATION_NUMBER_RESULT_DIV.style.color = "red";
-        COMPOSITE_CALCULATION_NUMBER_RESULT_DIV.innerHTML = `Empty Input Fields or Invalid Data`;
+        compositeCalculationNumberResultDiv.style.color = "red";
+        compositeCalculationNumberResultDiv.innerHTML = `Empty Input Fields or Invalid Data`;
     }
 });
