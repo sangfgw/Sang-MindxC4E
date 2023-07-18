73971d7b7fc3288ec6052b1fe76a2816e3a6cce1
sangfgw <sang050901i@gmail.com>
2023-07-18 10:37:28 +0700

    Exercise Week 8

--- js/index.js
+++ js/index.js
@@ -1,296 +1,116 @@
-/* 1. Driving Age Verification Program */
-const drivingAgeVerificationSubmitBtn = document.getElementById("driving_age_verification_submit");
-drivingAgeVerificationSubmitBtn.addEventListener("click", (ev) => {
-    const userAgeValue = document.getElementById("user_age").value === "" ? null : Number(document.getElementById("user_age").value);
-    const drivingAgeVerificationResultDiv = document.getElementById("driving_age_verification_result");
+/* 1. Sort Array By Ascending */
+const array = [
+    [{ name: "Hoàng", score: 2 }, { name: "Long", score: 4 }],
+    [{ name: "Tú", score: 100 }, { name: "Linh", score: 10 }],
+    [{ name: "Ngọc", score: 1 }, { name: "Long Lê", score: 9 }]
+];
 
-    if (!Number.isInteger(userAgeValue))
-    {
-        drivingAgeVerificationResultDiv.style.color = "red";
-        drivingAgeVerificationResultDiv.innerHTML = `Invalid Data (Input field must be an integer value)`;
-    }
-    else if (userAgeValue != null && userAgeValue < 1) {
-        drivingAgeVerificationResultDiv.style.color = "red";
-        drivingAgeVerificationResultDiv.innerHTML = `Invalid Data (Your age must be greater than 0)`;
-    }
-    else if (userAgeValue != null && isValidDrivingAge(userAgeValue)) {
-        drivingAgeVerificationResultDiv.style.color = "green";
-        drivingAgeVerificationResultDiv.innerHTML = `You are old enough to drive.`;
-    }
-    else if (userAgeValue != null) {
-        drivingAgeVerificationResultDiv.style.color = "darkorange";
-        drivingAgeVerificationResultDiv.innerHTML = `You are not old enough to drive.`;
-    }
-    else {
-        drivingAgeVerificationResultDiv.style.color = "red";
-        drivingAgeVerificationResultDiv.innerHTML = `Empty Input Field Or Invalid Data`;
-    }
-});
+// Define Function
+function sortArrASC(arr) {
+    // Create temp object array
+    const objArr = [];
 
-// Function: If age >= 18 (valid), else "Invalid"
-function isValidDrivingAge(age) {
-    return age >= 18 ? true : false;
-}
+    arr.forEach(item => {
+        if (typeof item == "object") {
 
-/* 2. Negative, Positive or Neutral number Checking */
-const numberCheckingSubmitBtn = document.getElementById("number_checking_submit");
-numberCheckingSubmitBtn.addEventListener("click", (ev) => {
-    const numberCheckingValue = document.getElementById("number_checking").value === "" ? null : Number(document.getElementById("number_checking").value);
-    const numberCheckingResultDiv = document.getElementById("number_checking_result");
-
-    if (numberCheckingValue != null && numberMarkVerify(numberCheckingValue) > 0) {
-        numberCheckingResultDiv.style.color = "green";
-        numberCheckingResultDiv.innerHTML = `${numberCheckingValue} is a positive number`;
-    }
-    else if (numberCheckingValue != null && numberMarkVerify(numberCheckingValue) === 0) {
-        numberCheckingResultDiv.style.color = "green";
-        numberCheckingResultDiv.innerHTML = `${numberCheckingValue} is a neutral number`;
-    }
-    else if (numberCheckingValue != null && numberMarkVerify(numberCheckingValue) < 0) {
-        numberCheckingResultDiv.style.color = "green";
-        numberCheckingResultDiv.innerHTML = `${numberCheckingValue} is a negative number`;
-    }
-    else {
-        numberCheckingResultDiv.style.color = "red";
-        numberCheckingResultDiv.innerHTML = `Empty Input Field Or Invalid Data`;
-    }
-});
-
-// Function: If number < 0 (return -1), else if number = 0 (return 0), else return 1
-function numberMarkVerify(number) {
-    return number < 0 ? -1 : number === 0 ? 0 : 1;
-}
-
-/* 3. Program checking whether input number can divide 3 and 5 or not */
-const numberDivideCheckingSubmitBtn = document.getElementById("number_divide_checking_submit");
-numberDivideCheckingSubmitBtn.addEventListener("click", (ev) => {
-    const numberDivideCheckingValue = document.getElementById("number_divide_checking").value === "" ? null : Number(document.getElementById("number_divide_checking").value);
-    const numberDivideCheckingResultDiv = document.getElementById("number_divide_checking_result");
-
-    if (numberDivideCheckingValue != null && isDivideThreeAndFive(numberDivideCheckingValue)) {
-        numberDivideCheckingResultDiv.style.color = "green";
-        numberDivideCheckingResultDiv.innerHTML = `Number ${numberDivideCheckingValue} can divide 3 and 5`;
-    }
-    else if (numberDivideCheckingValue != null && !isDivideThreeAndFive(numberDivideCheckingValue)) {
-        numberDivideCheckingResultDiv.style.color = "darkorange";
-        numberDivideCheckingResultDiv.innerHTML = `Number ${numberDivideCheckingValue} cannot divide 3 and 5`;
-    }
-    else {
-        numberDivideCheckingResultDiv.style.color = "red";
-        numberDivideCheckingResultDiv.innerHTML = `Empty Input Field Or Invalid Data`;
-    }
-});
-
-// Function: If number can divide 3 and 5 (valid), else (invalid)
-function isDivideThreeAndFive(number) {
-    return number % 3 === 0 && number % 5 === 0 ? true : false;
-}
-
-/* 4. Day of the Week Checking Program */
-const dayOfWeekNumberSubmitBtn = document.getElementById("day_of_week_number_submit");
-dayOfWeekNumberSubmitBtn.addEventListener("click", (ev) => {
-    const dayOfWeekNumber = document.getElementById("day_of_week_number").value === "" ? null : Number(document.getElementById("day_of_week_number").value);
-    const dayOfWeekNumberResultDiv = document.getElementById("day_of_week_number_result");
-
-    if (dayOfWeekNumber != null && dayOfWeek(dayOfWeekNumber) != null) {
-        dayOfWeekNumberResultDiv.style.color = "green";
-        dayOfWeekNumberResultDiv.innerHTML = `Number ${dayOfWeekNumber} corresponds to: ${dayOfWeek(dayOfWeekNumber)}`;
-    }
-    else if (dayOfWeekNumber != null && dayOfWeek(dayOfWeekNumber) === null) {
-        dayOfWeekNumberResultDiv.style.color = "red";
-        dayOfWeekNumberResultDiv.innerHTML = `Invalid Data, Your Input Field Must Be In Range 1 to 7`;
-    }
-    else {
-        dayOfWeekNumberResultDiv.style.color = "red";
-        dayOfWeekNumberResultDiv.innerHTML = `Empty Input Field Or Invalid Data`;
-    }
-});
-
-// Function: Return Day Of A Week
-function dayOfWeek(day) {
-    switch (day) {
-        case 1:
-            return "Monday";
-        case 2:
-            return "Tuesday";
-        case 3:
-            return "Wednesday";
-        case 4:
-            return "Thursday";
-        case 5:
-            return "Friday";
-        case 6:
-            return "Saturday";
-        case 7:
-            return "Sunday";
-        default:
-            return null;
-    }
-}
-
-/* 5. Sum of 1 to N */
-const sumNumberSubmitBtn = document.getElementById("sum_number_submit");
-sumNumberSubmitBtn.addEventListener("click", (ev) => {
-    const sumNumber = document.getElementById("sum_number").value === "" ? null : Number(document.getElementById("sum_number").value);
-    const sumNumberResultDiv = document.getElementById("sum_number_result");
-
-    if (sumNumber != null && Number.isInteger(sumNumber)) {
-        sumNumberResultDiv.style.color = "green";
-        sumNumberResultDiv.innerHTML = `Sum of 1 to ${sumNumber} is: ${sumOfOneToNumber(sumNumber)}`;
-    }
-    else if (sumNumber != null && !Number.isInteger(sumNumber)) {
-        sumNumberResultDiv.style.color = "red";
-        sumNumberResultDiv.innerHTML = `Invalid Data, Your Input Field Must Be An Integer Value`;
-    }
-    else {
-        sumNumberResultDiv.style.color = "red";
-        sumNumberResultDiv.innerHTML = `Empty Input Field Or Invalid Data`;
-    }
-});
-
-// Function: Calculate Sum of 1 to N
-function sumOfOneToNumber(number) {
-    let sum = 0;
-
-    for (let i = 1; i <= number; i++) {
-        sum += i;
-    }
-
-    return sum;
-}
-
-/* 6. Print List of Number With Skip Value Program */
-// Listen change Start Range Value
-const inputStartRangeNumber = document.getElementById("start_range_number");
-const currentStartRangeValueDiv = document.getElementById("current_start_range_value");
-currentStartRangeValueDiv.textContent = `Current Start Range Value: ${inputStartRangeNumber.value}`
-
-inputStartRangeNumber.addEventListener("input", (ev) => {
-    currentStartRangeValueDiv.textContent = `Current Start Range Value: ${ev.target.value}`;
-});
-
-// Listen change End Range value
-const inputEndRangeNumber = document.getElementById("end_range_number");
-const currentEndRangeValueDiv = document.getElementById("current_end_range_value");
-currentEndRangeValueDiv.textContent = `Current End Range Value: ${inputEndRangeNumber.value}`
-
-inputEndRangeNumber.addEventListener("input", (ev) => {
-    currentEndRangeValueDiv.textContent = `Current End Range Value: ${ev.target.value}`;
-});
-
-// Skip Number
-let skipNumber = document.getElementById("skip_number").value === "" ? null : Number.isInteger(Number(document.getElementById("skip_number").value)) ? Number(document.getElementById("skip_number").value) : null;
-
-// Listen Click Submit Button
-const rangeSkipNumberSubmitBtn = document.getElementById("range_skip_number_submit");
-const rangeSkipNumberResultDiv = document.getElementById("range_skip_number_result");
-
-rangeSkipNumberSubmitBtn.addEventListener("click", (ev) => {
-    skipNumber = document.getElementById("skip_number").value === "" ? null : Number.isInteger(Number(document.getElementById("skip_number").value)) ? Number(document.getElementById("skip_number").value) : null;
-    if (skipNumber != null) {
-        rangeSkipNumberResultDiv.style.color = "green";
-        rangeSkipNumberResultDiv.innerHTML = "";
-        printLoopSkipNumber(Number(inputStartRangeNumber.value), Number(inputEndRangeNumber.value), skipNumber);
-    }
-    else {
-        rangeSkipNumberResultDiv.style.color = "red";
-        rangeSkipNumberResultDiv.innerHTML = `Invalid Skip Number`;
-    }
-});
-
-// Print Only Once Skip Loop
-if (skipNumber != null) {
-    printLoopSkipNumber(Number(inputStartRangeNumber.value), Number(inputEndRangeNumber.value), skipNumber);
-}
-
-// Function: Loop From start to end number with skip value
-function printLoopSkipNumber(startNum, endNum, skipNum) {
-    for (; startNum <= endNum; startNum++) {
-        if (startNum === skipNum) {
-            continue;
+            // loop throught each object inside array
+            item.map(objItem => {
+                return objArr.push(objItem);
+            });
         }
-        rangeSkipNumberResultDiv.style.color = "green";
-        rangeSkipNumberResultDiv.innerHTML += `${startNum}<br>`;
-    }
+    });
 
-    // // While Method
-    // while (startNum <= endNum) {
-    //     if (startNum === skipNum) {
-    //         startNum++;
-    //         continue;
-    //     }
-    //     rangeSkipNumberResultDiv.style.color = "green";
-    //     rangeSkipNumberResultDiv.innerHTML += `${startNum}<br>`;
-    //     startNum++;
-    // }
+    // Sort Temp Array
+    objArr.sort((itemA, itemB) => {
+        if (itemA.score < itemB.score)
+            return -1;
+        if (itemA.score > itemB.score)
+            return 1;
+
+        // equal
+        return 0;
+    });
+
+    return objArr;
 }
 
-/* 7. Print List of Number Can Divide A given number Program */
-// Listen change Start Range Divide Value
-const inputStartRangeDivideNumber = document.getElementById("start_range_divide_number");
-const currentStartRangeDivideValueDiv = document.getElementById("current_start_range_divide_value");
-currentStartRangeDivideValueDiv.textContent = `Current Start Range Value: ${inputStartRangeDivideNumber.value}`
-
-inputStartRangeDivideNumber.addEventListener("input", (ev) => {
-    currentStartRangeDivideValueDiv.textContent = `Current Start Range Value: ${ev.target.value}`;
+// Use Function
+document.write(
+    `
+    <h1>1. Ascending Sort Score Array:</h1>
+    <div>[</div>
+`);
+sortArrASC(array).forEach((objItem, objIndex) => {
+    const keys = Object.keys(objItem);
+    const values = Object.values(objItem);
+    document.write(`
+        <div>&emsp;{</div>
+        <div>&emsp;&emsp;
+    `);
+    keys.forEach((key, keyIndex) => {
+        document.write(`<span>${keys[keyIndex]}: 
+        ${typeof values[keyIndex] === "string" ? "\"" : ""}${values[keyIndex]}${typeof values[keyIndex] === "string" ? "\"" : ""}
+        ${keyIndex === keys.length - 1 ? "" : ", "}</span>`);
+    });
+    document.write(`${objIndex === sortArrASC(array).length - 1 ? "</div><div>&emsp;}</div>" : "</div><div>&emsp;}, </div>"}`);
 });
+document.write(`<div>]</div>`);
 
-// Listen change End Range Divide value
-const inputEndRangeDivideNumber = document.getElementById("end_range_divide_number");
-const currentEndRangeDivideValueDiv = document.getElementById("current_end_range_divide_value");
-currentEndRangeDivideValueDiv.textContent = `Current End Range Value: ${inputEndRangeDivideNumber.value}`
+// document.write(
+//     `
+//     <h1>1. Ascending Sort Score Array:</h1>
+//     <p>${JSON.stringify(sortArrASC(array))}</p>
+// `);
 
-inputEndRangeDivideNumber.addEventListener("input", (ev) => {
-    currentEndRangeDivideValueDiv.textContent = `Current End Range Value: ${ev.target.value}`;
-});
+/* 2. Optional */
+const array2 = [
+    ["Bai_01", "Bai_02", "Bai_03", "Bai_04"],
+    ["Bai_01", "Bai_03"],
+    ["Bai_02", "Bai_04", "Bai_01"],
+    ["Bai_04", "Bai_02", "Bai_03", "Bai_01"],
+    ["Bai_04", "Bai_03", "Bai_02", "Bai_01"]
+];
 
-// Divide Number
-let divideNumber = document.getElementById("divide_number").value === "" ? null : Number.isInteger(Number(document.getElementById("divide_number").value)) ? Number(document.getElementById("divide_number").value) : null;
-
-// Listen Click Submit Button
-const rangeDivideNumberSubmitBtn = document.getElementById("range_divide_number_submit");
-const rangeDivideNumberResultDiv = document.getElementById("range_divide_number_result");
-
-rangeDivideNumberSubmitBtn.addEventListener("click", (ev) => {
-    divideNumber = document.getElementById("divide_number").value === "" ? null : Number.isInteger(Number(document.getElementById("divide_number").value)) ? Number(document.getElementById("divide_number").value) : null;
-    if (divideNumber != null && divideNumber > 0) {
-        rangeDivideNumberResultDiv.style.color = "green";
-        rangeDivideNumberResultDiv.innerHTML = "";
-        printFirstDivideNumber(Number(inputStartRangeDivideNumber.value), Number(inputEndRangeDivideNumber.value), divideNumber);
-    }
-    else if (divideNumber != null && divideNumber <= 0) {
-        rangeDivideNumberResultDiv.style.color = "red";
-        rangeDivideNumberResultDiv.innerHTML = `Divide Number Must Be A Positive Number and greater than 0`;
-    }
-    else {
-        rangeDivideNumberResultDiv.style.color = "red";
-        rangeDivideNumberResultDiv.innerHTML = `Invalid Divide Number`;
-    }
-});
-
-// Print Only Once Divide Loop
-if (divideNumber != null) {
-    printFirstDivideNumber(Number(inputStartRangeDivideNumber.value), Number(inputEndRangeDivideNumber.value), divideNumber);
-}
-
-// Function: Loop From start to end number and divide the given value if number % 2 === 0 then print it and break loop;
-function printFirstDivideNumber(startNum, EndNum, divideNum) {
-    rangeDivideNumberResultDiv.style.color = "green";
-    rangeDivideNumberResultDiv.innerHTML = `First Number That Can Divide ${divideNum}: `;
-
-    for (; startNum <= EndNum ; startNum++) {
-        if (startNum % divideNum === 0) {
-            rangeDivideNumberResultDiv.innerHTML += `${startNum}<br>`;
-            break;
+// Define Function
+function funcLessonObj(arr) {
+    const objArr = [];
+    arr.forEach(item => {
+        if (Array.isArray(item)) {
+            item.forEach((strItem, index) => {
+                if (index + 1 <= item.length - 1) {
+                    objArr.push({ [strItem]: item[index + 1] });
+                }
+            });
         }
-    }
+    });
 
-    // // While Loop Method
-    // while (startNum <= EndNum) {
-    //     if (startNum % divideNum === 0) {
-    //         rangeDivideNumberResultDiv.innerHTML += `${startNum}<br>`;
-    //         break;
-    //     }
-    //     startNum++;
-    // }
+    return objArr;
 }
+
+// Use Function
+document.write(
+    `
+    <h1>2. Lesson Object (Optional):</h1>
+    <div>{</div>
+`);
+
+funcLessonObj(array2).forEach((objItem, objIndex) => {
+    const keys = Object.keys(objItem);
+    const values = Object.values(objItem);
+    keys.forEach((key, keyIndex) => {
+        document.write(`<div>&emsp;${keys[keyIndex]}: 
+        ${typeof values[keyIndex] === "string" ? "\"" : ""}${values[keyIndex]}${typeof values[keyIndex] === "string" ? "\"" : ""}
+        ${objIndex === funcLessonObj(array2).length - 1 ? "" : ",<br>"}</div>`);
+    });
+});
+
+document.write(
+    `
+    <div>{</div>
+`);
+
+// document.write(
+//     `
+//     <h1>2. Lesson Object (Optional):</h1>
+//     <p>${JSON.stringify(funcLessonObj(array2))}</p>
+// `);
